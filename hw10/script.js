4ac2ce7e61ca5eeef16070719532bd5072744b7a
Iryna <irynka1071994@gmail.com>
2020-03-12 16:48:04 +0200

    hw10

---
+++ hw10/script.js
@@ -0,0 +1,30 @@
+const keyValues = ['q', 'w', 'e', 'r', 't', 'y', 'u'];
+
+const items = document.querySelectorAll('.item');
+items.forEach(item => {
+    item.addEventListener('click', () => {
+        playList(item);
+    });
+});
+
+document.addEventListener('keydown', e => {
+    if(e.repeat) return;
+    const item = e.key;
+    const itemIndex = keyValues.indexOf(item);
+    if(itemIndex > -1) playList(items[itemIndex]);
+});
+
+function playList(item) {
+    let song = document.getElementById(item.dataset.note);
+    song.play();
+    song.currentTime = 0;
+
+    document.addEventListener('play', e => {
+        if(song && song != e.target) {
+            song.pause();
+        }
+        song = e.target;
+    },
+    true
+    );
+}
